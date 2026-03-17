76a122418bb387b290721a8002e22a496ecf05e6
unknown <aulacenteg@gmail.com>
2026-03-17 20:46:42 -0300

    meu primeiro deploy

---
+++ retangulo/Program.vb
@@ -0,0 +1,26 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        
+        dim base As Double
+        dim altura As Double
+        dim area As Double
+
+        console.writeline("qual a base do retangulo?")
+        base= console.readline()
+
+        console.writeline("qual a altura do retangulo?")
+        altura= console.readline()
+
+        area= base * altura
+
+        console.writeline("a area do retangulo é " & area)
+        
+
+
+
+
+
+    End Sub
+End Module
