393670a9eeea5502f29f286391a8ac06a35232b4
gustavoevan <evangelistagustavo27@gmail.com>
2026-03-24 20:48:28 -0300

    primeiro deploy

---
+++ sistemas/Program.vb
@@ -0,0 +1,19 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        
+        dim os(3) as String
+
+        oS(0) = "linux"
+        os(1) = "windows"
+        os(2) = "ios"
+
+        console.writeline(os(0))
+        console.writeline(os(1))
+        console.writeline(os(2))
+         
+
+
+    End Sub
+End Module
