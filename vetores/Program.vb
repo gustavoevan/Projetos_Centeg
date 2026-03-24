b435676259201903cfd6e805891770e231065792
gustavoevan <evangelistagustavo27@gmail.com>
2026-03-24 20:30:35 -0300

    deploy errado

---
+++ vetores/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        
+        dim nomecompleto(2) as String
+
+        nomecompleto(0) = "gustavo"
+        nomecompleto(1) = "Evangelista"
+
+        console.writeline(nomecompleto(0)  &  nomecompleto(1))
+        
+
+
+    End Sub
+End Module
